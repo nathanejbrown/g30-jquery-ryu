9d5a902b57e2349e9b40b710d78185f2996b704b
Heathcliff <dennis.nathan@gmail.com>
2016-07-18 14:37:43 -0600

    Uploaded files, edited script.js to fulfill requirements

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
-    // append an img tag with our picture of Ryu
-    // url: http://i.imgur.com/90Mmdcm.png
+    $('#ryu').append("<img src='http://i.imgur.com/90Mmdcm.png'>");
 
-    // change the img src on hover to the animated gif of Ryu
-    // url: http://i.imgur.com/nTj3Fxx.gif
-    // when the user 'unhovers' change back to static Ryu
+$('#ryu > img').hover(function(){
+  this.src = 'http://i.imgur.com/nTj3Fxx.gif';
+}, function(){
+  this.src = 'http://i.imgur.com/90Mmdcm.png';
+})
 
-    // when a user clicks, change Ryu's stance
-    // url: http://i.imgur.com/Rfj0a80.png
+$('#ryu > img').mousedown(function() {
+  this.src = 'http://i.imgur.com/Rfj0a80.png';
+}),
+$('#ryu > img').mouseup(function(){
+  this.src = 'http://i.imgur.com/90Mmdcm.png';
+})
 
-    // add the Hadouken!
-    // url: http://i.imgur.com/oTyQRvX.gif
+$('#ryu').click(function(){
+  $('#ryu').append("<img class='demo-hadouken' src='http://i.imgur.com/oTyQRvX.gif'>");
+  $('.demo-hadouken').css('position', 'absolute');
+  $('.demo-hadouken').animate({
+    left: '100vw'
+  }, 1000, function() {
+    this.remove("<img class='demo-hadouken'>");
+  })
 
-    // animate that Hadouken
+})
 
-    // let Ryu relax
-    // url: http://i.imgur.com/90Mmdcm.png
 
 });
